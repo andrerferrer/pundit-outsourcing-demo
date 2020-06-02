56a0830895deca1f474f9b2729bc722dd45bf1e4
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 20:28:24 -0300

    Rails generate offers controller with index and show

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,7 @@
+class OffersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'offers#index'
+  resources :offers, only: %i[ index ]
 end
