f733005d4caf2789fd5f60ffe5d43f828525e6d6
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 22:45:14 -0300

    Implement the booking policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,12 +1,20 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      # The logic here is:
+      # You can only see the bookings that you are the customer
+      # OR
+      # The bookings that you are the owner of the offer
+      bookings_user_is_customer = scope.where(customer: user)
+      bookins_user_is_owner = scope.joins(:offer).where(offers: { owner: user } )
+
+      bookings_user_is_customer + bookins_user_is_owner
     end
   end
 
   # the 'easy' solution is implemented here and in the offers#index view as a conditional (if/else)
+  # we will implement this in the BookingsController#index as well
   def index?
-
+    record.customer == user || record.offer.owner == user
   end
 end
