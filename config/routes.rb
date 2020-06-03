765963bff157a5d76d013b8da0c37d0b43180f60
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 22:44:25 -0300

    change route to bookings#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'offers#index'
+  root to: 'bookings#index'
   resources :offers, only: %i[ index show ]
   resources :bookigns, only: :index
 end
