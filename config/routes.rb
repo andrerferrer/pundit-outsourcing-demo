c23f1e7c23234eeb1ff04a253c8d438af06d6cf8
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 22:26:06 -0300

    Rewrite routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
   devise_for :users
   root to: 'offers#index'
   resources :offers, only: %i[ index show ]
+  resources :bookigns, only: :index
 end
