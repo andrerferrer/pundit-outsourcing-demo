0003034ddd0ff55dcb5691cee6f62a8014665bfe
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 23:08:39 -0300

    Implement bookings#create logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,22 @@ class BookingsController < ApplicationController
     # BookingPolicy::Scope#resolve
     @bookings = policy_scope(Booking)
   end
+
+  def create
+    @offer = Offer.find params[:offer_id]
+    @booking = Booking.new(
+      customer: current_user,
+      offer: @offer
+    )
+    
+    # Create the booking if the user is not the owner 
+    authorize @booking
+  
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'offers/show'
+    end
+
+  end
 end
