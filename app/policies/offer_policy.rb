226a0521022ebedceffea11a8f6cc74aff427274
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 21:10:44 -0300

    Implement the hard solution

--- app/policies/offer_policy.rb
+++ app/policies/offer_policy.rb
@@ -1,6 +1,14 @@
 class OfferPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
+      # Allow the user to see it only if the user is the owner or the customer
+      
+      # the 'hard' solution is implemented here
+      # scope.joins(:bookings).where(owner: user)
+      #      .or(
+      #        scope.joins(:bookings).where(bookings: { customer: user }
+      #      ))
+
       scope.all
     end
   end
