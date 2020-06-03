4f00eb918611d40fcf209ded8acb1386f38e7d45
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 22:44:38 -0300

    implement the index logic on bookings#index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
   def index
+    # The back-end protection is implemented in the
+    # BookingPolicy::Scope#resolve
+    @bookings = policy_scope(Booking)
   end
 end
