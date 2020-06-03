40ba2969ddec12dd727366b9185566027d53c7ee
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 22:48:32 -0300

    Fix typo and add bookings#create in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'bookings#index'
   resources :offers, only: %i[ index show ]
-  resources :bookigns, only: :index
+  resources :bookings, only: %i[ index create ]
 end
